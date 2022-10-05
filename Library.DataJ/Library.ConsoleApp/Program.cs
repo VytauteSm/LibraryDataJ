7784bfd575e5e25b0ded1f9ceb38cb60a75726d5
VytauteSm <113047310+VytauteSm@users.noreply.github.com>
2022-10-05 12:04:17 +0300

    Add files via upload

---
+++ Library.DataJ/Library.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using Library.Data;
+
+namespace Library.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bookLibrary = new BookLibrary();
+
+            var books = XmlBookHelper.Load();
+
+            books.ForEach(book =>
+            {
+                bookLibrary.Books.Add(book);
+            });
+                
+
+            while (true)
+            {
+                var menuSelection = Menu.GetUserMenuSelection();
+
+                switch (menuSelection)
+                {
+                    case "A":
+                        AddBookUIHelper.Start(bookLibrary);
+                        break;
+                    case "R":
+                        RemoveBookUIHelper.Start(bookLibrary);
+                        break;
+                    case "S":
+                        SearchHelper.Start(bookLibrary);
+                        break;
+                    case "E":
+                        return;
+                }
+            }
+        }
+    }
+}
---
+++ Library.DataJ/Library.Data/BookLibrary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Data
+{
+    public class BookLibrary
+    {
+        public BookLibrary()
+        {
+            Load();
+        }
+
+        private List<Book> _books = new List<Book>();
+
+        public List<Book> Books { get { return _books; } } //added get access to book list for searching and removing
+
+        public void Add(Book book)
+        {
+            _books.Add(book);
+        }
+
+        public void Remove(Book book)
+        {
+            _books.Remove(book);
+        }
+
+        private void Load()
+        {
+            _books.Add(new Book("Test title")
+            {
+                Description = "Description",
+                Rating = 3,
+                Genre = GenreEnum.ScienceFiction,
+                Authors = new List<Author>() { new Author("Nick") }.ToArray(),
+                Keywords = new string[] { "Old", "England" }
+            });
+
+            _books.Add(new Book("Horror one")
+            {
+                Description = "Some description",
+                Rating = 5,
+                Genre = GenreEnum.Horror,
+                Authors = new List<Author>() { new Author("Nick"), new Author("John", "Smith") }.ToArray(), //added more than one author for testing purposes
+                Keywords = new string[] { "Old", "England" }
+            });
+        }
+    }
+}
